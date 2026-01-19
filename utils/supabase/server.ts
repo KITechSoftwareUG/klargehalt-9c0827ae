3b8a52e3f4e9b779f578deb102b8a217b25d48a4
AyhamAlk <aalkh@kitech-software.de>
2026-01-19 14:47:14 +0100

    Fix cookieStore await for Next.js 15 and add typescript-eslint dep

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -10,12 +10,14 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
         supabaseKey!,
         {
             cookies: {
-                getAll() {
-                    return cookieStore.getAll()
+                async getAll() {
+                    const store = await cookieStore;
+                    return store.getAll()
                 },
-                setAll(cookiesToSet) {
+                async setAll(cookiesToSet) {
                     try {
-                        cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+                        const store = await cookieStore;
+                        cookiesToSet.forEach(({ name, value, options }) => store.set(name, value, options))
                     } catch {
                         // The `setAll` method was called from a Server Component.
                         // This can be ignored if you have middleware refreshing
